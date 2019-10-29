98c9a6f487f4d9f01d0df032b5a87b68b7a34047
sehaj99 <sehajmeetsingh.sohal@sjsu.edu>
2019-10-29 15:20:01 -0700

    Made some changes

---
+++ Chess Game Model/src/edu.sjsu.cs151.model/GameEvent.java
@@ -0,0 +1,5 @@
+package edu.sjsu.cs151.model;
+
+public enum GameEvent {
+	WhiteWin, BlackWin
+}
---
+++ Chess Game Model/src/edu.sjsu.cs151.model/Player.java
@@ -0,0 +1,22 @@
+package edu.sjsu.cs151.model;
+/**
+ * 
+ * Created by Neeval Kumar on 10/27/2019
+ *
+ */
+public class Player {
+public String color;
+public boolean firstTurn = false;
+
+public Player() {
+	
+}
+
+public String getColor() {
+	return color;
+}
+
+public boolean firstTurn() {
+	return firstTurn = true;
+}
+}
