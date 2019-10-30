c41c082f7906fb6534448b162ae791b70e0a54e1
sehaj99 <sehajmeetsingh.sohal@sjsu.edu>
2019-10-30 13:28:48 -0700

    We initialized the color.

--- Chess Game/src/edu/sjsu/cs151/model/Piece.java
+++ Chess Game/src/edu/sjsu/cs151/model/Piece.java
@@ -1,85 +1,98 @@
 package edu.sjsu.cs151.model;
+
 /**
- * Created by Sehajmeet on 10/26/2019
+ * Created by Sehaj on 10/26/2019
  */
 public abstract class Piece {
 
-	//Piece needs to hold type, color and name
+	// Piece needs to hold type, color and name
 	private PieceType type;
 	private PieceColor color;
 	boolean white = false;
 	boolean PieceAlive = true;
-	
-	
-	public Piece(boolean white,PieceType type) {
+
+	public Piece(boolean white, PieceType type) {
 		this.white = white;
+		if (white == true) {
+			color = PieceColor.White;
+		} else
+			color = PieceColor.Black;
 		this.type = type;
 
-		
 	}
-	
-	
-	//enums for type and color
-	public enum PieceColor{
-		White, Black
+
+	// enums for type and color
+	public enum PieceColor {
+		White {
+			@Override
+			public String toString() {
+				return "w";
+			}
+		},
+
+		Black {
+			@Override
+			public String toString() {
+				return "w";
+			}
+		}
 	}
-	
-	public enum PieceType{
+
+	public enum PieceType {
 		King {
 			@Override
-            public String toString() {
-                return "K";
-            }
-		}, 
+			public String toString() {
+				return "K";
+			}
+		},
 		Queen {
 			@Override
-            public String toString() {
-            return "Q";
-           } 
-		}, 
-		Rook{
+			public String toString() {
+				return "Q";
+			}
+		},
+		Rook {
 			@Override
-            public String toString() {
-                return "R";
-            }
-		}, Knight{
+			public String toString() {
+				return "R";
+			}
+		},
+		Knight {
 			@Override
-            public String toString() {
-                return "K";
-            }
+			public String toString() {
+				return "K";
+			}
+		},
+		Bishop {
+			@Override
+			public String toString() {
+				return "B";
+			}
+		},
+		Pawn {
+			@Override
+			public String toString() {
+				return "P";
+			}
 		}
-		, Bishop{
-			@Override
-            public String toString() {
-                return "B";
-            }
-		}
-		, Pawn{
-			@Override
-            public String toString() {
-                return "P";
-            }
-		}
-		
-		
+
 	}
-	
+
 	public boolean isWhite() {
 		return this.white == true;
 	}
 
-	//getter methods for each
+	// getter methods for each
 	public PieceType getPieceType() {
 		return type;
 	}
-	
-	
+
 	public PieceColor getPieceColor() {
 		return color;
 	}
-	
+
 	public abstract boolean isValidMove(Board board, Tile currentPos, Tile endPos);
-	
+
 	public boolean pieceDied() {
 		this.PieceAlive = false;
 		return this.PieceAlive;
