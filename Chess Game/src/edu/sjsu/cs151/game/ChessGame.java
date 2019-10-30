1a4854d1942abad21f8073174251216d31d5ef71
Neev <neevalkumar@gmail.com>
2019-10-29 20:51:46 -0700

    Rename ChessGame Model depository to just Chess Game

---
+++ Chess Game/src/edu/sjsu/cs151/game/ChessGame.java
@@ -0,0 +1,17 @@
+package edu.sjsu.cs151.game;
+import java.util.Scanner;
+
+import edu.sjsu.cs151.model.*;
+public class ChessGame {
+	
+	public static void main(String args[]) {
+		System.out.println("For this assignment we are simulating a chess game in console.");
+		Game game = new Game();
+		game.printBoard();
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+	}
+
+}
