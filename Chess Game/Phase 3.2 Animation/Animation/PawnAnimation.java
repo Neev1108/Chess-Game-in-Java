3c38934c60720c03abeed63db37f6ffc7332fc78
sehaj99 <sehajmeetsingh.sohal@sjsu.edu>
2019-11-06 14:05:16 -0800

    Added a ReadMe file and made some changes.

--- Chess Game/Phase 3.2 Animation/Animation/PawnAnimation.java
+++ Chess Game/Phase 3.2 Animation/Animation/PawnAnimation.java
@@ -11,18 +11,14 @@ public class PawnAnimation {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		frame.setSize(2000, 2000);
 
 		final MoveableShape shape = new PawnShape(0, 0, PAWN_WIDTH);
 		final MoveableShape quiz = new stationaryTile(0, 0, 0);
-		final MoveableShape bish = new Bishop(0,0, PAWN_WIDTH);
-
-
-	
-		
+		final MoveableShape bish = new Bishop(0, 0, PAWN_WIDTH);
 
 		ShapeIcon icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
 		final JLabel label = new JLabel(icon);
-		frame.setSize(2000, 2000);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(label);
@@ -38,11 +34,7 @@ public class PawnAnimation {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(label2);
-		
-		
-		
 
-		
 		final int DELAY = 5;
 
 		Timer t = new Timer(DELAY, event -> {
@@ -54,21 +46,19 @@ public class PawnAnimation {
 		});
 
 		t.start();
-		
+
 		final int FIELD_WIDTH = 20;
 		final JTextField textField = new JTextField(FIELD_WIDTH);
-		
-		
-		
+
 		JButton startButton = new JButton("START CHESS GAME");
 		startButton.addActionListener(event -> textField.setText("Press the start button!"));
-		
-        JPanel pnl = new JPanel(new GridBagLayout()); 
 
-        pnl.add(startButton);
-        frame.add(pnl);
+		JPanel pnl = new JPanel(new GridBagLayout());
+
+		pnl.add(startButton);
+		frame.add(pnl);
 		frame.setVisible(true);
-		
+
 	}
 
 }
---
+++ Chess Game/Phase 3.2 Animation/Animation/ReadMe.txt
@@ -0,0 +1,2 @@
+Hey,
+Please run java swing animation a couple of times  because swing becomes buggy. If buggy please close swing and run it again. 
