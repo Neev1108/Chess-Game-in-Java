9766546d877f686847563a20960b7c39a9e1e24d
sehaj99 <sehajmeetsingh.sohal@sjsu.edu>
2019-11-26 17:03:13 -0800

    Hi

---
+++ Chess Game/src/edu/sjsu/cs151/game/Game.java
@@ -0,0 +1,21 @@
+package edu.sjsu.cs151.game;
+
+import edu.sjsu.cs151.view.*;
+import edu.sjsu.cs151.model.*;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import edu.sjsu.cs151.controller.*;
+
+public class Game {
+
+	private static BlockingQueue<Message> queue = new LinkedBlockingQueue<Message>();
+	private static View view;
+	private static Model model;
+
+	public static void main(String [] args){
+	
+	}
+
+}
