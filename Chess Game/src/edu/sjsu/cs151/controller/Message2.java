9b6c0164c199e1904fa07274994251b1bb675773
sehaj99 <sehajmeetsingh.sohal@sjsu.edu>
2019-12-02 11:02:28 -0800

    HI

---
+++ Chess Game/src/edu/sjsu/cs151/controller/Message2.java
@@ -0,0 +1,8 @@
+package edu.sjsu.cs151.controller;
+import java.io.Serializable;
+
+/** Represents events/messages sent from View to Model **/
+
+public abstract class Message2 implements Serializable {
+	private static final long serialVersionUID = 1L;
+}
