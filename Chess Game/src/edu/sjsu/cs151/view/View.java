297c44416fed9ed2cb17e84e4147f617a3be9ec9
Skyler Hungerford <skyler.hungerford@sjsu.edu>
2019-12-04 11:59:06 -0800

    Modified View so that it bypasses the start animation. Hopefully will avoid freezing

--- Chess Game/src/edu/sjsu/cs151/view/View.java
+++ Chess Game/src/edu/sjsu/cs151/view/View.java
@@ -54,50 +54,50 @@ public class View implements Runnable {
 		 * Creating a JFrame of size 2000 x 2000
 		 */
 
-		JFrame frame = new JFrame();
+/*		JFrame frame = new JFrame();
 		frame.setSize(1000, 500);
-
+*/
 		/**
 		 * Creating three MoveableShape objects
 		 */
-		final MoveableShape shape = new PawnShape(0, 0, PAWN_WIDTH);
+/*		final MoveableShape shape = new PawnShape(0, 0, PAWN_WIDTH);
 		final MoveableShape quiz = new stationaryTile(0, 0, 100);
 		final MoveableShape bish = new BishopShape(0, 0, PAWN_WIDTH);
-
+*/
 		/**
 		 * Creating the first chess piece animation
 		 */
-		ShapeIcon icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
+/*		ShapeIcon icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
 		final JLabel label = new JLabel(icon);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(label);
-
+*/
 		/**
 		 * Creating the second chess piece animation
 		 */
-		ShapeIcon bishop = new ShapeIcon(bish, ICON_WIDTH, ICON_HEIGHT);
+/*		ShapeIcon bishop = new ShapeIcon(bish, ICON_WIDTH, ICON_HEIGHT);
 		final JLabel label3 = new JLabel(bishop);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.add(label3);
-
+*/
 		/**
 		 * Creating the tiles of the chess board
 		 */
-		ShapeIcon tile = new ShapeIcon(quiz, ICON_WIDTH, ICON_HEIGHT);
+/*		ShapeIcon tile = new ShapeIcon(quiz, ICON_WIDTH, ICON_HEIGHT);
 		final JLabel label2 = new JLabel(tile);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(label2);
 
 		final int DELAY = 5;
-
+*/
 		/**
 		 * The timer for the animation
 		 */
-		Timer t = new Timer(DELAY, event -> {
+/*		Timer t = new Timer(DELAY, event -> {
 			shape.move();
 			label.repaint();
 			bish.move();
@@ -106,14 +106,14 @@ public class View implements Runnable {
 		});
 
 		t.start();
-
+*/
 		/**
 		 * Creating the button for starting the chess game
 		 */
-		JButton startButton = new JButton("START CHESS GAME");
+//		JButton startButton = new JButton("START CHESS GAME");
 
 		// Creating a GridBagLayout for our output
-		JLabel backg = new JLabel(new ImageIcon("background.png"));
+/*		JLabel backg = new JLabel(new ImageIcon("background.png"));
 		backg.setLayout(new FlowLayout());
 		JPanel pnl = new JPanel();
 
@@ -123,13 +123,13 @@ public class View implements Runnable {
 		frame.setVisible(true);
 
 		startButton.addActionListener(event -> {
-
+*/
 			// was doing some thread testing lock.wait will make the view thread wait, but I
 			// still have to figure out how to start the model's thread
 
 			// synchronized(lock) {
 			// try {
-			try {
+/*			try {
 				queue.put(new NewGameMessage());// Add to queue
 				
 				printMessageQueue();
@@ -148,8 +148,9 @@ public class View implements Runnable {
 
 			playerScreen();
 
-		});
+		});*/
 
+		playerScreen();
 	}
 
 	public static void playerScreen() {
