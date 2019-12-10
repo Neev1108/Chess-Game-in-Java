63d04511d06bf75a506429d231d24cffdddbee1f
Skyler Hungerford <skyler.hungerford@sjsu.edu>
2019-12-10 05:58:21 -0800

    Network is a real pain

--- Chess Game/src/edu/sjsu/cs151/controller/Client.java
+++ Chess Game/src/edu/sjsu/cs151/controller/Client.java
@@ -2,10 +2,14 @@ package edu.sjsu.cs151.controller;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
 	
-	
+	public static void main(String args[]) throws Exception
+	{
+		
+	}
 	
 	
 }
